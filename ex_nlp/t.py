0625e0a6b7134ad9c921d48e43215b12f3e7182f
hong <napolehong92@naver.com>
2023-04-04 16:29:47 +0900

    230404_크롤링

---
+++ ex_nlp/t.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+import pandas as pd
+import openpyxl
+import os
+
+query = "네이버 mou"
+url_template = "https://www.google.com/search?q={}&tbm=nws&start={}"
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(options=options)
+# driver.maximize_window()
+
+data = [] 
+for page in range(200,300):
+    url = url_template.format(query, page)
+    driver.get(url)
+    time.sleep(5)  # 페이지 로딩을 위해 5초 대기
+    articles = driver.find_elements(By.CLASS_NAME, 'mCBkyc.ynAwRc.MBeuO.nDgy9d')
+    print(f'-------------{page}page---------------')
+    for i in articles :
+        print(i.text)
+        data.append([i.text])  # 데이터 추가
+
+driver.quit()
+
+file_name = time.strftime("%Y%m%d_%H%M%S") + '.xlsx'  # 현재 시간 정보를 이용하여 파일 이름 생성
+path = os.path.join(r'C:\Users\seo\Desktop', file_name)  # 폴더 경로와 파일 이름 결합
+
+try:
+    df = pd.read_excel(path, sheet_name='sheet1')  # 기존 데이터 읽기
+    df = pd.concat([df, pd.DataFrame(data, columns=['text'])], ignore_index=True)  # 새로운 데이터 추가
+    
+except FileNotFoundError:
+    df = pd.DataFrame(data, columns=['text'])  # 데이터프레임 생성
+    with pd.ExcelWriter(path) as writer:
+        df.to_excel(writer, sheet_name='sheet1', index=False)
+
+with pd.ExcelWriter(path, engine='openpyxl', mode='a') as writer:
+    df.to_excel(writer, sheet_name='sheet1', index=False, header=not writer.sheets['sheet1'])
+
